d643b61c16de56df18bc183e1de0cdbb129d2a0c
omozuas <iyanuomozua@gmail.com>
2024-08-12 10:02:30 +0100

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_boilerplate_hng11/screens/signup_screen.dart';
+import 'package:flutter_boilerplate_hng11/features/auth/screen/signup_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -15,6 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(textTheme: GoogleFonts.interTextTheme()),
-        home:  const SignupScreen());
+        home: const SignupScreen());
   }
 }
