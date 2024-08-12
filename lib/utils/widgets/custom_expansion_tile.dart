083cc413fd88b4117cfdc7ab2299076903b9006b
Rejoice-official <100317000+Rejoice-official@users.noreply.github.com>
2024-08-12 14:43:46 +0100

    updated custom expansion_tile widget

--- lib/utils/widgets/custom_expansion_tile.dart
+++ lib/utils/widgets/custom_expansion_tile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_boilerplate_hng11/utils/globalColors/global_colors.dart';
 import 'package:google_fonts/google_fonts.dart';
+import '../global_colors.dart';
 
 class CustomExpansionTile extends StatefulWidget {
   const CustomExpansionTile({
