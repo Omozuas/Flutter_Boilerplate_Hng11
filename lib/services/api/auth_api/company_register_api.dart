c1b7b2d430bd94f8da1e46946f3527fe66a780d0
Okelola Joshua <jayokelola341@gmail.com>
2024-08-12 12:12:25 +0100

    changes to company_register_api.

--- lib/services/api/auth_api/company_register_api.dart
+++ lib/services/api/auth_api/company_register_api.dart
@@ -5,21 +5,23 @@ import 'package:flutter_boilerplate_hng11/utils/error/error.dart';
 import 'package:http/http.dart' as http;
 
 Future<Company> registerUser(Company user) async {
-  final url = Uri.parse('https://api-golang.boilerplate.hng.tech/api/v1/organisations');
-  var company = Company.initial();
+  const baseUrl = 'https://api-golang.boilerplate.hng.tech/api/v1';
+  var initialCompanyModel = Company.initial();
   try {
+    final url = Uri.parse('$baseUrl/organisations');
     final response = await http.post(
       url,
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NfdXVpZCI6IjAxOTE0NWFhLWQ0MGUtN2I2OS05ZTAyLTNhM2JmNzZiMDBjNyIsImF1dGhvcmlzZWQiOnRydWUsImV4cCI6MTcyMzYyMzUwOSwidXNlcl9pZCI6IjAxOTE0MzQ3LTY4OTYtN2I2MC1hN2ExLTQ1ZDdiNTg3N2UyNiJ9.GZPVi0Wc08uoFC-jeLVzig6xAo1_CO3gXn_8tDioGx8'
+        'Authorization':
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NfdXVpZCI6IjAxOTE0NWFhLWQ0MGUtN2I2OS05ZTAyLTNhM2JmNzZiMDBjNyIsImF1dGhvcmlzZWQiOnRydWUsImV4cCI6MTcyMzYyMzUwOSwidXNlcl9pZCI6IjAxOTE0MzQ3LTY4OTYtN2I2MC1hN2ExLTQ1ZDdiNTg3N2UyNiJ9.GZPVi0Wc08uoFC-jeLVzig6xAo1_CO3gXn_8tDioGx8'
       },
       body: jsonEncode(user.toJson()),
     );
 
     if (response.statusCode == 201) {
       // Organization created successfully
-      company = Company.fromJson(response.body);
+      initialCompanyModel = Company.fromJson(response.body);
     } else if (response.statusCode == 400) {
       // Bad request
       throw ApiError(
@@ -47,5 +49,5 @@ Future<Company> registerUser(Company user) async {
     // Handle any exceptions that might occur during the request
     debugPrint('Registration failed: ${e.toString()}');
   }
-  return company;
+  return initialCompanyModel;
 }
