1f52b19fb74593da88268b4257c413baacde5540
roseline oladiran <roselineoladiran97@gmail.com>
2024-08-13 05:59:19 -0700

    updated my branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_boilerplate_hng11/features/auth/screen/signup_screen.dart';
-import 'package:flutter_boilerplate_hng11/features/user_setting/screens/user_screen.dart';
 import 'package:flutter_boilerplate_hng11/services/service_locator.dart';
+
+import 'package:flutter_boilerplate_hng11/utils/routing/app_router.dart';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-
+void main() async {
   setupLocator();
-
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -17,13 +19,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      ensureScreenSize: true,
-      designSize: const Size(390, 844),
-      builder: (context, child) => MaterialApp(
+    return ProviderScope(
+      child: ScreenUtilInit(
+        ensureScreenSize: true,
+        designSize: const Size(390, 844),
+        builder: (context, child) => MaterialApp.router(
+          routerConfig: AppRouter.router,
           debugShowCheckedModeBanner: false,
-          theme: ThemeData(textTheme: GoogleFonts.interTextTheme()),
-          home:  SignupScreen()),
+          theme: ThemeData(
+            textTheme: GoogleFonts.interTextTheme(),
+          ),
+        ),
+      ),
     );
   }
 }
