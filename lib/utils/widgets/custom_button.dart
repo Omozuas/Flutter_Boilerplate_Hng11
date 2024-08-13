36a1a5459ae75283e7eecbfe13ad3cd755010bfa
Mofolasayo Osikoya <osikoyamofolasayo@gmail.com>
2024-08-13 16:56:35 +0100

    quality assurance

--- lib/utils/widgets/custom_button.dart
+++ lib/utils/widgets/custom_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+// ignore: must_be_immutable
 class CustomButton extends StatelessWidget {
   final VoidCallback onTap;
   final Color containerColor, borderColor, textColor;
