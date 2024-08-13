8db668f634b46466418fb7335e9d62085be6c07c
fidelity2851 <ukwuanifidelis2851@gmail.com>
2024-08-13 12:38:36 +0100

    Synced from feature branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate_hng11/features/auth/screen/signup_screen.dart';
+import 'package:flutter_boilerplate_hng11/features/auth/screen/splash_screen.dart';
 import 'package:flutter_boilerplate_hng11/services/service_locator.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
