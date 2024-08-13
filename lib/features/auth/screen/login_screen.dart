d0c836f789b9b7e501c931520d647fa2fb6ce890
Rayhan Awojobi <dexterousnorthpaw@gmail.com>
2024-08-13 10:03:51 +0100

    login screen

---
+++ lib/features/auth/screen/login_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(
+              "Log In",
+              style: GoogleFonts.inter(color: Color(0xFF141414), fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
