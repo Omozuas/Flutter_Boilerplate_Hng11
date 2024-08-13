1278fa5936cda3e030a8b75302bc47855f2823a8
damilare-d <damilareakinwale0@gmail.com>
2024-08-13 20:50:00 +0100

    UI: added routes for the member screen

--- lib/utils/routing/app_router.dart
+++ lib/utils/routing/app_router.dart
@@ -2,12 +2,13 @@ import 'package:flutter_boilerplate_hng11/features/auth/screen/company_signup_sc
 import 'package:flutter_boilerplate_hng11/features/auth/screen/login_screen.dart';
 import 'package:flutter_boilerplate_hng11/features/auth/screen/regular_signup_screen.dart';
 import 'package:flutter_boilerplate_hng11/features/auth/screen/splash_screen.dart';
+import 'package:flutter_boilerplate_hng11/features/user_setting/screens/organisational_settings/members.dart';
 import 'package:flutter_boilerplate_hng11/utils/routing/consumer_go_router.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
-    initialLocation: AppRoute.splash,
+    initialLocation: AppRoute.membersSettings,
     routes: [
       ConsumerGoRoute(
         path: AppRoute.splash,
@@ -33,6 +34,12 @@ class AppRouter {
           return const LoginScreen();
         },
       ),
+      ConsumerGoRoute(
+        path: AppRoute.membersSettings,
+        builder: (context, state, ref) {
+          return const MembersSettings();
+        },
+      ),
     ],
   );
 }
@@ -42,6 +49,7 @@ class AppRoute {
   static const String companySignUp = '/companySignUp';
   static const String regularSignUp = '/regularSignUp';
   static const String login = '/login';
+  static const String membersSettings = '/membersSettings';
 }
 
 
