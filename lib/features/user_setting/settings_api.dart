a169868af218cd50b673ef66703bda7332eac1c2
Damigrace1 <ogdamtim@gmail.com>
2024-08-13 01:48:01 +0100

    refactor: Rename some files

--- lib/features/user_setting/settings_api.dart
+++ lib/features/user_setting/settings_api.dart
@@ -1,11 +1,11 @@
 import '../../services/dio_provider.dart';
 import '../../services/service_locator.dart';
 
-class ProductApi {
+class SettingsApi {
   //Inject the DioProvider Dependency
   DioProvider dioProvider = locator<DioProvider>();
 
-//You can start creating product listing functions now
+//You can start creating account settings functions now
 
 
 }
