50499d685ac75d87b21823d1f8b9fed823494b2e
Jamiu Jimoh <hyperjay55@gmail.com>
2024-08-13 11:43:36 +0100

    feat: Implement EditProfile screen

---
+++ lib/features/user_setting/widgets/empty_avatar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EmptyAvatar extends StatelessWidget {
+  const EmptyAvatar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/images/empty_avatar.png');
+  }
+}
