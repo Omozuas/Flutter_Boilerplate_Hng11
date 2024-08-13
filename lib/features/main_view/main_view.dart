bf887e415ace07bca784a97397e436420df75fde
Christian Onyisi <krysnkem@gmail.com>
2024-08-13 17:41:39 +0100

    implement bottom navigation with provider

--- lib/features/main_view/main_view.dart
+++ lib/features/main_view/main_view.dart
@@ -1,45 +1,24 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate_hng11/features/main_view/main_view_provider.dart';
 import 'package:flutter_boilerplate_hng11/utils/icons/nav_bar_icons.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:back_button_interceptor/back_button_interceptor.dart';
 
-class MainView extends StatefulWidget {
+class MainView extends ConsumerStatefulWidget {
   const MainView({super.key, required this.navigationShell});
   final StatefulNavigationShell navigationShell;
 
   final String name = 'MainView';
 
-  void goBranch(int index) {
-    navigationShell.goBranch(
-      index,
-      initialLocation: index == navigationShell.currentIndex,
-    );
-  }
-
   @override
-  State<MainView> createState() => _MainViewState();
+  ConsumerState<MainView> createState() => _MainViewState();
 }
 
-class _MainViewState extends State<MainView> {
-  int _bottomBardIndex = 0;
-  // Initial selected drawer item
+class _MainViewState extends ConsumerState<MainView> {
   final _scaffoldkey = GlobalKey<ScaffoldState>();
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _bottomBardIndex = index;
-      widget.goBranch(index);
-      _scaffoldkey.currentState?.closeDrawer();
-    });
-  }
-
-  bool myInterceptor(bool stopDefaultButtonEvent, RouteInfo info) {
-    if (widget.navigationShell.currentIndex != 0) {
-      _onItemTapped(0);
-      return true;
-    }
-    return false;
-  }
 
   @override
   void initState() {
@@ -58,33 +37,63 @@ class _MainViewState extends State<MainView> {
     super.dispose();
   }
 
+  void goBranch(WidgetRef ref, int index) {
+    final currentIndex = ref.read(bottomNavBarIndexProvider);
+    widget.navigationShell.goBranch(
+      index,
+      initialLocation: index == currentIndex,
+    );
+    ref.read(bottomNavBarIndexProvider.notifier).setIndex(index);
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
+    final bottomBarIndex = ref.watch(bottomNavBarIndexProvider);
+
     return Scaffold(
-        key: _scaffoldkey,
-        body: widget.navigationShell,
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(NavBar.home),
-              label: 'Home',
+      key: _scaffoldkey,
+      body: widget.navigationShell,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              NavBar.home,
             ),
-            BottomNavigationBarItem(
-              icon: Icon(NavBar.products),
-              label: 'Products',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(NavBar.cart),
-              label: 'Cart',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(NavBar.settings),
-              label: 'Settings',
-            ),
-          ],
-          currentIndex: _bottomBardIndex,
-          selectedItemColor: Colors.amber[800],
-          onTap: _onItemTapped,
-        ));
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(NavBar.products),
+            label: 'Products',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(NavBar.cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(NavBar.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: bottomBarIndex,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        onTap: (index) {
+          goBranch(ref, index);
+        },
+      ),
+    );
+  }
+
+  FutureOr<bool> myInterceptor(
+      bool stopDefaultButtonEvent, RouteInfo routeInfo) {
+    if (widget.navigationShell.currentIndex != 0) {
+      goBranch(ref, 0);
+      return true;
+    }
+    return false;
   }
 }
