16bfb95b57747d1460671e308c3b8b4fdbbb9331
JGraphics <jejelovegraphics@gmail.com>
2024-08-13 09:18:00 -0700

    setting up

---
+++ lib/features/auth/screen/forgot_password.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
