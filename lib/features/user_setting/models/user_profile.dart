69046a539c3edb22794aeea9323983dd45798aa3
Jamiu Jimoh <hyperjay55@gmail.com>
2024-08-14 09:05:17 +0100

    feat: Added user model

---
+++ lib/features/user_setting/models/user_profile.dart
@@ -0,0 +1,135 @@
+import 'dart:convert';
+
+class UserProfile {
+  final String id;
+  final DateTime createdAt;
+  final DateTime? updatedAt;
+  final String? username;
+  final String? jobTitle;
+  final String? pronouns;
+  final String? department;
+  final String email;
+  final String? bio;
+  final List<String> socialLinks;
+  final String? language;
+  final String? region;
+  final String? timezones;
+  final String? profilePicURL;
+  final DateTime? deletedAt;
+
+  const UserProfile({
+    required this.id,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.username,
+    required this.jobTitle,
+    required this.pronouns,
+    required this.department,
+    required this.email,
+    required this.bio,
+    required this.socialLinks,
+    required this.language,
+    required this.region,
+    required this.timezones,
+    required this.profilePicURL,
+    required this.deletedAt,
+  });
+
+  UserProfile copyWith({
+    String? id,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    String? username,
+    String? jobTitle,
+    String? pronouns,
+    String? department,
+    String? email,
+    String? bio,
+    List<String>? socialLinks,
+    String? language,
+    String? region,
+    String? timezones,
+    String? profilePicURL,
+    DateTime? deletedAt,
+  }) {
+    return UserProfile(
+      id: id ?? this.id,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      username: username ?? this.username,
+      jobTitle: jobTitle ?? this.jobTitle,
+      pronouns: pronouns ?? this.pronouns,
+      department: department ?? this.department,
+      email: email ?? this.email,
+      bio: bio ?? this.bio,
+      socialLinks: socialLinks ?? this.socialLinks,
+      language: language ?? this.language,
+      region: region ?? this.region,
+      timezones: timezones ?? this.timezones,
+      profilePicURL: profilePicURL ?? this.profilePicURL,
+      deletedAt: deletedAt ?? this.deletedAt,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'created_at': createdAt.toIso8601String(),
+      if (updatedAt != null) 'updated_at': updatedAt!.toIso8601String(),
+      if (username != null) 'username': username,
+      if (jobTitle != null) 'jobTitle': jobTitle,
+      if (pronouns != null) 'pronouns': pronouns,
+      if (department != null) 'department': department,
+      'email': email,
+      if (bio != null) 'bio': bio,
+      'social_links': socialLinks,
+      if (language != null) 'language': language,
+      if (region != null) 'region': region,
+      if (timezones != null) 'timezones': timezones,
+      if (profilePicURL != null) 'profile_pic_url': profilePicURL,
+      if (deletedAt != null) 'deletedAt': deletedAt!.toIso8601String(),
+    };
+  }
+
+  factory UserProfile.fromMap(Map<String, dynamic> map) {
+    return UserProfile(
+      id: map['id'] as String,
+      createdAt: DateTime.parse(map['created_at'] ?? ''),
+      updatedAt: DateTime.tryParse(map['updated_at'] ?? ''),
+      username: map['username'] as String?,
+      jobTitle: map['jobTitle'] as String?,
+      pronouns: map['pronouns'] as String?,
+      department: map['department'] as String?,
+      email: map['email'] as String,
+      bio: map['bio'] as String?,
+      socialLinks: List<String>.from((map['social_links'] ?? [])),
+      language: map['language'] as String?,
+      region: map['region'] as String?,
+      timezones: map['timezones'] as String?,
+      profilePicURL: map['profile_pic_url'] as String?,
+      deletedAt: DateTime.tryParse(map['deletedAt'] ?? ''),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserProfile.fromJson(String source) =>
+      UserProfile.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'UserProfile(id: $id, createdAt: $createdAt, updatedAt: $updatedAt, username: $username, jobTitle: $jobTitle, pronouns: $pronouns, department: $department, email: $email, bio: $bio, socialLinks: $socialLinks, language: $language, region: $region, timezones: $timezones, profilePicURL: $profilePicURL, deletedAt: $deletedAt)';
+  }
+
+  @override
+  bool operator ==(covariant UserProfile other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode;
+  }
+}
--- lib/features/user_setting/screens/organisational_settings/integrations_screen.dart
+++ lib/features/user_setting/screens/organisational_settings/integrations_screen.dart
@@ -1,1 +1,153 @@
+import 'dart:convert';
 
+import 'user_owned_organization.dart';
+import 'user_profile.dart';
+
+class User {
+  final String id;
+  final DateTime createdAt;
+  final DateTime? updatedAt;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String? status;
+  final String? phone;
+  final bool isActive;
+  final List<String> backupCodes;
+  final int? attemptsLeft;
+  final int? timeLeft;
+  final String? secret;
+  final bool is2FAEnabled;
+  final DateTime? deletedAt;
+  final UserProfile profile;
+  final List<UserOwnedOrganization> organisations;
+
+  const User({
+    required this.id,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.status,
+    required this.phone,
+    required this.isActive,
+    required this.backupCodes,
+    required this.attemptsLeft,
+    required this.timeLeft,
+    required this.secret,
+    required this.is2FAEnabled,
+    required this.deletedAt,
+    required this.profile,
+    required this.organisations,
+  });
+
+  User copyWith({
+    String? id,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    String? firstName,
+    String? lastName,
+    String? email,
+    String? status,
+    String? phone,
+    bool? isActive,
+    List<String>? backupCodes,
+    int? attemptsLeft,
+    int? timeLeft,
+    String? secret,
+    bool? is2FAEnabled,
+    DateTime? deletedAt,
+    UserProfile? profile,
+    List<UserOwnedOrganization>? organisations,
+  }) {
+    return User(
+      id: id ?? this.id,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      email: email ?? this.email,
+      status: status ?? this.status,
+      phone: phone ?? this.phone,
+      isActive: isActive ?? this.isActive,
+      backupCodes: backupCodes ?? this.backupCodes,
+      attemptsLeft: attemptsLeft ?? this.attemptsLeft,
+      timeLeft: timeLeft ?? this.timeLeft,
+      secret: secret ?? this.secret,
+      is2FAEnabled: is2FAEnabled ?? this.is2FAEnabled,
+      deletedAt: deletedAt ?? this.deletedAt,
+      profile: profile ?? this.profile,
+      organisations: organisations ?? this.organisations,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'created_at': createdAt.toIso8601String(),
+      if (updatedAt != null) 'updated_at': updatedAt!.toIso8601String(),
+      'first_name': firstName,
+      'last_name': lastName,
+      'email': email,
+      if (status != null) 'status': status,
+      if (phone != null) 'phone': phone,
+      'is_active': isActive,
+      'backup_codes': backupCodes,
+      if (attemptsLeft != null) 'attempts_left': attemptsLeft,
+      if (timeLeft != null) 'time_left': timeLeft,
+      if (secret != null) 'secret': secret,
+      'is_2fa_enabled': is2FAEnabled,
+      if (deletedAt != null) 'deletedAt': deletedAt!.toIso8601String(),
+      'profile': profile.toMap(),
+      'owned_organisations': organisations.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id'] as String,
+      createdAt: DateTime.parse(map['created_at'] ?? ''),
+      updatedAt: DateTime.tryParse(map['updated_at'] ?? ''),
+      firstName: map['first_name'] as String,
+      lastName: map['last_name'] as String,
+      email: map['email'] as String,
+      status: map['status'] as String,
+      phone: map['phone'] as String,
+      isActive: map['is_active'] as bool,
+      backupCodes: List<String>.from((map['backup_codes'] ?? [])),
+      attemptsLeft: map['attempts_left'] as int?,
+      timeLeft: map['time_left'] as int?,
+      secret: map['secret'] as String,
+      is2FAEnabled: map['is_2fa_enabled'] as bool,
+      deletedAt: DateTime.tryParse(map['deletedAt'] ?? ''),
+      profile: UserProfile.fromMap(map['profile'] as Map<String, dynamic>),
+      organisations: List<UserOwnedOrganization>.from(
+        (map['owned_organisations'] as List<Map<String, dynamic>>)
+            .map(UserOwnedOrganization.fromMap),
+      ),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) =>
+      User.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'User(id: $id, createdAt: $createdAt, updatedAt: $updatedAt, firstName: $firstName, lastName: $lastName, email: $email, status: $status, phone: $phone, isActive: $isActive, backupCodes: $backupCodes, attemptsLeft: $attemptsLeft, timeLeft: $timeLeft, secret: $secret, is2FAEnabled: $is2FAEnabled, deletedAt: $deletedAt, profile: $profile, organization: $organisations)';
+  }
+
+  @override
+  bool operator ==(covariant User other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode;
+  }
+}
