26431c333ca8fb58fb60633738dec127a36c8c22
Christian Onyisi <krysnkem@gmail.com>
2024-08-15 17:31:51 +0100

    create contract for product api

--- lib/features/product_listing/product_api.dart
+++ lib/features/product_listing/product_api.dart
@@ -1,9 +1,54 @@
+import 'package:flutter_boilerplate_hng11/features/product_listing/product_endpoints.dart';
+
 import '../../services/dio_provider.dart';
 import '../../services/service_locator.dart';
 
-class ProductApi {
+class ProductApi implements ProductsApiContract {
   //Inject the DioProvider Dependency
   DioProvider dioProvider = locator<DioProvider>();
+  
+  @override
+  Future createProduct({required Map product}) async{
+  }
+  
+  @override
+  Future deleteProduct({required String id}) {
+    // TODO: implement deleteProduct
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future getAllProducts() {
+    // TODO: implement getAllProducts
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future updateProduct({required String id, required product}) {
+    // TODO: implement updateProduct
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future getProduct({required String id}) {
+        return dioProvider.get(productsByIdEndpoint(id: id));
+
+  }
 
 //You can start creating product listing functions now
 }
+
+
+abstract class ProductsApiContract {
+  Future getAllProducts();
+
+  Future createProduct({required covariant dynamic product});
+  Future updateProduct({required String id, required covariant dynamic product});
+  Future deleteProduct({required String id});
+  Future getProduct({required String id});
+}
+
+
+
+
+
