488f8304cb6c42c4407dba9d1a1e78af509e38c1
Jamiu Jimoh <hyperjay55@gmail.com>
2024-08-16 14:35:11 +0100

    feat: Integrated profile avatar upload api

--- lib/features/user_setting/provider/profile_provider.dart
+++ lib/features/user_setting/provider/profile_provider.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
-import 'dart:io';
 
-import 'package:flutter_boilerplate_hng11/features/user_setting/models/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
 
+import '../models/user_model.dart';
 import '../models/user_profile.dart';
 import '../settings_api.dart';
 
@@ -11,10 +11,28 @@ class ProfileProvider extends AutoDisposeNotifier<ProfileProviderStates> {
   @override
   ProfileProviderStates build() {
     return const ProfileProviderStates(
+      pickedImage: null,
       user: AsyncData(null),
       profileUpdater: AsyncData(null),
       profileAvatarUpdater: AsyncData(null),
     );
+  }
+
+  Future<void> pickImage(ImageSource? source) async {
+    if (source == null) {
+      state = state.copyWith(pickedImage: null);
+
+      return;
+    }
+    final XFile? image = await ImagePicker().pickImage(source: source);
+
+    if (image != null) {
+      state = state.copyWith(pickedImage: image);
+      return;
+    }
+
+    state = state.copyWith(pickedImage: null);
+    return;
   }
 
   Future<void> getUser() async {
@@ -32,6 +50,7 @@ class ProfileProvider extends AutoDisposeNotifier<ProfileProviderStates> {
   Future<void> updateProfile({
     required String email,
     required UserProfile profile,
+    XFile? image,
   }) async {
     final settingsApi = ref.read(settingsApiProvider);
     try {
@@ -40,6 +59,12 @@ class ProfileProvider extends AutoDisposeNotifier<ProfileProviderStates> {
         email: email,
         profile: profile,
       );
+      if (image != null) {
+        await settingsApi.updateProfileAvatar(
+          email: email,
+          image: image,
+        );
+      }
       await getUser();
       state = state.copyWith(profileUpdater: AsyncData(res));
     } catch (e) {
@@ -49,7 +74,7 @@ class ProfileProvider extends AutoDisposeNotifier<ProfileProviderStates> {
 
   Future<void> updateProfileAvatar({
     required String email,
-    required File image,
+    required XFile image,
   }) async {
     final settingsApi = ref.read(settingsApiProvider);
     try {
@@ -72,22 +97,26 @@ final profileProvider =
         ProfileProvider.new);
 
 class ProfileProviderStates {
+  final XFile? pickedImage;
   final AsyncValue<UserModel?> user;
   final AsyncValue<UserProfile?> profileUpdater;
   final AsyncValue<String?> profileAvatarUpdater;
 
   const ProfileProviderStates({
+    required this.pickedImage,
     required this.user,
     required this.profileUpdater,
     required this.profileAvatarUpdater,
   });
 
   ProfileProviderStates copyWith({
+    XFile? pickedImage,
     AsyncValue<UserModel?>? user,
     AsyncValue<UserProfile?>? profileUpdater,
     AsyncValue<String?>? profileAvatarUpdater,
   }) {
     return ProfileProviderStates(
+      pickedImage: pickedImage ?? this.pickedImage,
       user: user ?? this.user,
       profileUpdater: profileUpdater ?? this.profileUpdater,
       profileAvatarUpdater: profileAvatarUpdater ?? this.profileAvatarUpdater,
