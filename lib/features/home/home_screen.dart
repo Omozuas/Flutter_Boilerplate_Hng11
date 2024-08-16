610c9103dcd6f762e0080a5e96e9a7ddbd6a4427
Michael <michaelcee2000@gmail.com>
2024-08-16 19:13:53 +0100

    edited

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -26,145 +26,176 @@ class HomeScreen extends ConsumerWidget {
       SalesData('May', 100, 150, 200),
       SalesData('Jun', 50, 100, 250),
     ];
-    return Scaffold(
-      appBar: AppBar(
+    return SafeArea(
+      child: Scaffold(
         backgroundColor: GlobalColors.white,
-        bottomOpacity: 1,
-        leading: Padding(
-          padding: EdgeInsets.only(left: 26.0.w),
-          child: const CircleAvatar(
-            backgroundImage: AssetImage('assets/images/logo.png'),
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(right: 8.0.w),
-            child: Stack(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.notifications),
-                ),
-                const Positioned(
-                  top: 0.0,
-                  right: 0.0,
-                  child: Badge(
-                    label:
-                        Text('5'), // Replace with the actual notification count
-                    textColor: Colors.white,
-                    backgroundColor: Colors.red,
-                    textStyle: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-      backgroundColor: GlobalColors.white,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+        body: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'Dashboard',
-                style: TextStyle(fontSize: 24.sp, fontWeight: FontWeight.bold),
-              ),
               SizedBox(
-                height: GlobalScreenSize.getScreenHeight(context) * 0.008,
-              ),
-              Text(
-                'This Month\'s Summary',
-                style: TextStyle(
-                  fontSize: 16.sp,
-                  color: GlobalColors.gray600Color,
-                ),
-              ),
+                  height: GlobalScreenSize.getScreenHeight(context) * 0.015),
               SizedBox(
-                height: GlobalScreenSize.getScreenHeight(context) * 0.020,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
+                height: GlobalScreenSize.getScreenHeight(context) * 0.050,
+                width: double.infinity,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24.0.w),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const RevenueCard(
-                        title: 'Total Revenue',
-                        value: '\$20000',
-                        percentageChange: '+15% decrease',
+                      CircleAvatar(
+                        radius: 25.w,
+                        backgroundImage:
+                            const AssetImage('assets/images/logo.png'),
                       ),
-                      SizedBox(
-                        width: GlobalScreenSize.getScreenWidth(context) * 0.018,
-                      ),
-                      const RevenueCard(
-                        title: 'Total Revenue',
-                        value: '\$20000',
-                        percentageChange: '+65% decrease',
-                      ),
-                      SizedBox(
-                        width: GlobalScreenSize.getScreenWidth(context) * 0.018,
-                      ),
-                      const RevenueCard(
-                        title: 'Total Revenue',
-                        value: '\$20000',
-                        percentageChange: '+45% decrease',
-                      ),
-                      SizedBox(
-                        width: GlobalScreenSize.getScreenWidth(context) * 0.018,
-                      ),
-                      const RevenueCard(
-                        title: 'Total Revenue',
-                        value: '\$20000',
-                        percentageChange: '+25% decrease',
-                      ),
-                      SizedBox(
-                        width: GlobalScreenSize.getScreenWidth(context) * 0.018,
-                      ),
-                      const RevenueCard(
-                        title: 'Total Revenue',
-                        value: '\$20000',
-                        percentageChange: '+75% decrease',
+                      const Spacer(),
+                      Padding(
+                        padding: EdgeInsets.only(right: 8.0.w),
+                        child: Stack(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.notifications, size: 25),
+                            ),
+                            Positioned(
+                              top: 5.h,
+                              right: 10.w,
+                              child: Badge(
+                                isLabelVisible: true,
+                                label: const Text('5'),
+                                textColor: GlobalColors.white,
+                                backgroundColor: GlobalColors.red,
+                                textStyle: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
               ),
-              Row(
-                children: [
-                  Text(
-                    'Overview',
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16.sp),
+              SizedBox(
+                  height: GlobalScreenSize.getScreenHeight(context) * 0.016),
+              Divider(
+                color: GlobalColors.gray500Color,
+                thickness: 0.2,
+              ),
+              SizedBox(
+                  height: GlobalScreenSize.getScreenHeight(context) * 0.016),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+                child: Text(
+                  'Dashboard',
+                  style:
+                      TextStyle(fontSize: 24.sp, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: GlobalScreenSize.getScreenHeight(context) * 0.008,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+                child: Text(
+                  'This Month\'s Summary',
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    color: GlobalColors.gray600Color,
                   ),
-                  const Spacer(),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'See more',
-                      style: TextStyle(
-                        color: GlobalColors.gray600Color,
-                      ),
+                ),
+              ),
+              SizedBox(
+                height: GlobalScreenSize.getScreenHeight(context) * 0.020,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 24.w),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const RevenueCard(
+                          title: 'Total Revenue',
+                          value: '\$20000',
+                          percentageChange: '+15% decrease',
+                        ),
+                        SizedBox(
+                          width:
+                              GlobalScreenSize.getScreenWidth(context) * 0.018,
+                        ),
+                        const RevenueCard(
+                          title: 'Subscription',
+                          value: '\$20000',
+                          percentageChange: '+65% decrease',
+                        ),
+                        SizedBox(
+                          width:
+                              GlobalScreenSize.getScreenWidth(context) * 0.018,
+                        ),
+                        const RevenueCard(
+                          title: 'Total Revenue',
+                          value: '\$20000',
+                          percentageChange: '+45% decrease',
+                        ),
+                        SizedBox(
+                          width:
+                              GlobalScreenSize.getScreenWidth(context) * 0.018,
+                        ),
+                        const RevenueCard(
+                          title: 'Total Revenue',
+                          value: '\$20000',
+                          percentageChange: '+25% decrease',
+                        ),
+                        SizedBox(
+                          width:
+                              GlobalScreenSize.getScreenWidth(context) * 0.018,
+                        ),
+                        const RevenueCard(
+                          title: 'Total Revenue',
+                          value: '\$20000',
+                          percentageChange: '+75% decrease',
+                        ),
+                      ],
                     ),
-                  )
-                ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+                child: Row(
+                  children: [
+                    Text(
+                      'Overview',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16.sp),
+                    ),
+                    const Spacer(),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'See more',
+                        style: TextStyle(
+                          color: GlobalColors.gray600Color,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
 
               // Overview (Bar Chart)
-              Container(
-                padding: EdgeInsets.all(24.w),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0.w),
                 child: SfCartesianChart(
                   backgroundColor: Colors.white,
                   plotAreaBorderColor: Colors.transparent,
                   primaryXAxis: CategoryAxis(
                     majorGridLines: const MajorGridLines(width: 0),
                     axisLine: const AxisLine(width: 0),
-                    // Add label style customization
                     labelStyle: TextStyle(
                       color: Colors.grey[600],
                       fontSize: 12.sp,
@@ -174,14 +205,13 @@ class HomeScreen extends ConsumerWidget {
                     majorGridLines: MajorGridLines(width: 0),
                     minorGridLines: MinorGridLines(width: 0),
                     axisLine: AxisLine(width: 0),
-                    // Add axis label
                   ),
                   series: <CartesianSeries>[
                     ColumnSeries<SalesData, String>(
                       dataSource: data,
                       xValueMapper: (SalesData data, _) => data.month,
                       yValueMapper: (SalesData data, _) => data.veryGood,
-                      color: const Color(0xFFE0E0E0),
+                      color: GlobalColors.gray600Color,
                       name: 'Very Good',
                       borderRadius: const BorderRadius.only(
                         topLeft: Radius.circular(10),
@@ -193,67 +223,71 @@ class HomeScreen extends ConsumerWidget {
                       dataSource: data,
                       xValueMapper: (SalesData data, _) => data.month,
                       yValueMapper: (SalesData data, _) => data.good,
-                      color: const Color(0xFFFFC107),
+                      color: GlobalColors.lightOrangeColor,
                       name: 'Good',
                       borderRadius: const BorderRadius.only(
                         topLeft: Radius.circular(10),
                         topRight: Radius.circular(10),
                       ),
-                      width: 0.7, // Reduce the bar width to allow overlapping
+                      width: 0.7, 
                     ),
                     ColumnSeries<SalesData, String>(
                       dataSource: data,
                       xValueMapper: (SalesData data, _) => data.month,
                       yValueMapper: (SalesData data, _) => data.poor,
-                      color: const Color(0xFFC70039),
+                      color: GlobalColors.orange,
                       name: 'Poor',
                       borderRadius: const BorderRadius.only(
                         topLeft: Radius.circular(10),
                         topRight: Radius.circular(10),
                       ),
-                      width: 0.7, // Reduce the bar width to allow overlapping
+                      width: 0.7, 
                     ),
                   ],
                 ),
               ),
 
               // Recent Sales
-              SizedBox(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        const Text(
-                          'Recent Sales',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        const Spacer(),
-                        TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'See more',
-                            style: TextStyle(
-                              color: GlobalColors.gray600Color,
-                            ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+                child: SizedBox(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          const Text(
+                            'Recent Sales',
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
-                        )
-                      ],
-                    ),
-                    ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: customers.length,
-                      itemBuilder: (context, index) {
-                        final customer = customers[index];
-                        return CustomerListTile(
-                          customerName: customer['name'] ?? 'Unknown Customer',
-                          email: customer['email'] ?? 'No Email Provided',
-                          amount: customer['amount'] ?? '0.00',
-                        );
-                      },
-                    ),
-                  ],
+                          const Spacer(),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'See more',
+                              style: TextStyle(
+                                color: GlobalColors.gray600Color,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: customers.length,
+                        itemBuilder: (context, index) {
+                          final customer = customers[index];
+                          return CustomerListTile(
+                            customerName:
+                                customer['name'] ?? 'Unknown Customer',
+                            email: customer['email'] ?? 'No Email Provided',
+                            amount: customer['amount'] ?? '0.00',
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
