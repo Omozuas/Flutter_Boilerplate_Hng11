fcceaa30b2ea166f644326df61e7b4c366b73a75
Damigrace1 <ogdamtim@gmail.com>
2024-08-16 08:31:54 +0100

    pulled changes from features

--- lib/services/custom_interceptor.dart
+++ lib/services/custom_interceptor.dart
@@ -4,11 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate_hng11/features/auth/screen/login_screen.dart';
 import 'package:flutter_boilerplate_hng11/services/error_handlers.dart';
 import 'package:flutter_boilerplate_hng11/services/service_locator.dart';
+import 'package:flutter_boilerplate_hng11/utils/routing/app_router.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:go_router/go_router.dart';
 import 'package:one_context/one_context.dart';
 
 class CustomInterceptor extends Interceptor {
   GetStorage box = locator<GetStorage>();
+
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
     ErrorHandlers.allErrorHandler(err);
@@ -24,24 +27,23 @@ class CustomInterceptor extends Interceptor {
     if (box.read('accessToken') != null) {
       options.headers["Authorization"] = "Bearer ${box.read('accessToken')}";
     }
+    else if (options.path == 'auth/login'
+        || options.path == '/auth/google?mobile=true'
+        || options.path == '/auth/register'
+    ){
+
+    }
     else{
-      if(options.path == 'auth/login'){}
-      else{
-        Navigator.push(OneContext().context!, MaterialPageRoute(builder:
-            (context)=>const LoginScreen()
-        ));
-      }
-
-      //when token is expired has been handled in the error handle as when status code is 401.
-
+      OneContext().push(MaterialPageRoute(builder: (context)=>LoginScreen()));
+      handler.reject(
+        DioException(
+          requestOptions: options,
+          type: DioExceptionType.cancel,
+          error: 'Access token is required',
+        ),
+      );
     }
     super.onRequest(options, handler);
   }
 
-  @override
-  void onResponse(Response response, ResponseInterceptorHandler handler) {
-    // log('Response >> ${response}');
-
-    super.onResponse(response, handler);
-  }
 }
