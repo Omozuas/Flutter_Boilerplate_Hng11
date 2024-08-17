dab0e1e565a79ac7f2f99829746b15e95c1324ef
omozuas <iyanuomozua@gmail.com>
2024-08-17 14:31:05 +0100

    update

--- lib/features/user_setting/screens/profile_settings/account_settings.dart
+++ lib/features/user_setting/screens/profile_settings/account_settings.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate_hng11/features/user_setting/provider/profile_provider.dart';
-import 'package:flutter_boilerplate_hng11/features/user_setting/widgets/dialogs/delete_member_dialog.dart';
 import 'package:flutter_boilerplate_hng11/features/user_setting/widgets/profile_avatar.dart';
 import 'package:flutter_boilerplate_hng11/utils/global_colors.dart';
 import 'package:flutter_boilerplate_hng11/utils/routing/app_router.dart';
