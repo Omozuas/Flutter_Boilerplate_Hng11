d7d0d0aea26825496620f6207f4ac8dfbaaf6b0b
omozuas <iyanuomozua@gmail.com>
2024-08-17 14:29:36 +0100

    update

--- lib/features/user_setting/screens/profile_settings/account_settings.dart
+++ lib/features/user_setting/screens/profile_settings/account_settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate_hng11/features/user_setting/provider/profile_provider.dart';
+import 'package:flutter_boilerplate_hng11/features/user_setting/widgets/dialogs/delete_member_dialog.dart';
 import 'package:flutter_boilerplate_hng11/features/user_setting/widgets/profile_avatar.dart';
 import 'package:flutter_boilerplate_hng11/utils/global_colors.dart';
 import 'package:flutter_boilerplate_hng11/utils/routing/app_router.dart';
@@ -194,9 +195,16 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
                       SizedBox(height: 8.h),
                       InkWell(
                         onTap: () {
-                          stotage.remove('accessToken');
-
-                          context.go(AppRoute.login);
+                          // showDialog(
+                          //   context: context,
+                          //   builder: (context) => LogOutDialog(
+                          //     onTap: () {
+                          //       stotage.remove('accessToken');
+                          //       Navigator.pop(context);
+                                context.go(AppRoute.login);
+                          //     },
+                          //   ),
+                          // );
                         },
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
