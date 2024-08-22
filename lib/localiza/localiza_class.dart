fa04f6708bb9c94ba228f0e636123818df10f8d3
fidelmak <holubakin@gmail.com>
2024-08-22 09:33:53 +0100

    updates

--- lib/localiza/localiza_class.dart
+++ lib/localiza/localiza_class.dart
@@ -4,17 +4,5 @@ class LocalizationClass {
   static Locale english = const Locale('en');
   static Locale chinese = const Locale('zh');
 
-
   static List<Locale> locales = [english, chinese];
 }
-
-
-  static Locale english =  const Locale('en');
-  static Locale chinese = const  Locale('zh');
-
-  static List<Locale> locales = [
-    english,chinese
-  ];
-
-}
-
