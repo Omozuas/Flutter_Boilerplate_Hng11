dadf1c4540335a318c4e069bcffc06fb30b9070d
ellatechie <111292475+Dev-ellaij@users.noreply.github.com>
2024-08-23 15:13:48 +0100

    Update single_user_signup.dart

---
+++ lib/features/auth/screen/single_user_signup.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate_hng11/features/auth/widgets/loading_overlay.dart';
+import 'package:flutter_boilerplate_hng11/utils/custom_text_style.dart';
+import 'package:flutter_boilerplate_hng11/utils/routing/app_router.dart';
+import 'package:flutter_boilerplate_hng11/utils/widgets/password_textfield.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import '../../../utils/global_colors.dart';
+import '../../../utils/validator.dart';
+import '../../../utils/widgets/custom_button.dart';
+import '../../../utils/widgets/custom_text_field.dart';
+import '../providers/auth.provider.dart';
+import 'package:flutter_boilerplate_hng11/l10n/app_localizations.dart';
+
+class SingleUserSignUpScreen extends ConsumerWidget {
+  const SingleUserSignUpScreen({super.key});
+
+  static final firstNameController = TextEditingController();
+  static final lastNameController = TextEditingController();
+  static final emailController = TextEditingController();
+  static final passwordController = TextEditingController();
+  static final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authProviderState = ref.watch(authProvider);
+
+    return LoadingOverlay(
+      isLoading: authProviderState.normalButtonLoading ||
+          authProviderState.googleButtonLoading,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 76.h),
+            child: FocusScope(
+              child: Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Text(
+                      AppLocalizations.of(context)!.signUpTitle,
+                      style: TextStyle(
+                        fontSize: 28.w,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10.h),
+                    Text(
+                      AppLocalizations.of(context)!.signUpSubtitle,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(height: 30.h),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                        padding: EdgeInsets.symmetric(vertical: 12.h),
+                        side: const BorderSide(color: Colors.grey),
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(4)),
+                        ),
+                      ),
+                      onPressed: () {
+                        ref.read(authProvider.notifier).googleSignin(context);
+                      },
+                      child: authProviderState.googleButtonLoading
+                          ? SizedBox(
+                              width: 16.w,
+                              height: 25.w,
+                              child: CircularProgressIndicator.adaptive(
+                                strokeWidth: 2.w,
+                              ),
+                            )
+                          : Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'assets/images/googleIcon.png',
+                                  fit: BoxFit.contain,
+                                  width: 25.w,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  AppLocalizations.of(context)!.googleButton,
+                                  style: CustomTextStyle.medium(
+                                      fontSize: 17.sp,
+                                      color: GlobalColors.dark2),
+                                )
+                              ],
+                            ),
+                    ),
+                    SizedBox(
+                      height: 24.h,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(width: 105.w, child: const Divider()),
+                        const Spacer(),
+                        Text(
+                          AppLocalizations.of(context)!.continueWithText,
+                          style: CustomTextStyle.regular(
+                            color: GlobalColors.darkOne,
+                          ),
+                        ),
+                        const Spacer(),
+                        SizedBox(width: 105.w, child: const Divider()),
+                      ],
+                    ),
+                    SizedBox(height: 10.h),
+                    CustomTextField(
+                      label: AppLocalizations.of(context)!.firstName,
+                      controller: SingleUserSignUpScreen.firstNameController,
+                      hintText: AppLocalizations.of(context)!.enterFirstName,
+                      focusedBorderColor: GlobalColors.orange,
+                      validator: Validators.nameValidator,
+                    ),
+                    CustomTextField(
+                      label: AppLocalizations.of(context)!.lastName,
+                      controller: lastNameController,
+                      hintText: AppLocalizations.of(context)!.enterLastName,
+                      focusedBorderColor: GlobalColors.orange,
+                      validator: Validators.nameValidator,
+                    ),
+                    CustomTextField(
+                      label: AppLocalizations.of(context)!.email,
+                      controller: SingleUserSignUpScreen.emailController,
+                      hintText: AppLocalizations.of(context)!.enterEmail,
+                      focusedBorderColor: GlobalColors.orange,
+                      validator: Validators.emailValidator,
+                    ),
+                    PasswordTextField(
+                      label: AppLocalizations.of(context)!.password,
+                      controller: SingleUserSignUpScreen.passwordController,
+                      hintText: AppLocalizations.of(context)!.createPassword,
+                      obscureText: true,
+                      focusedBorderColor: GlobalColors.orange,
+                      validator: Validators.passwordValidator,
+                    ),
+                    SizedBox(height: 10.h),
+                    CustomButton(
+                      text: AppLocalizations.of(context)!.createAccountButton,
+                      loading: authProviderState.normalButtonLoading,
+                      onTap: () async {
+                        if (formKey.currentState!.validate()) {
+                          _handleCreateAccount(ref, context);
+                        }
+                      },
+                      textColor: GlobalColors.white,
+                      borderColor: GlobalColors.orange,
+                      height: 55.h,
+                      containerColor: GlobalColors.orange,
+                      width: 50,
+                    ),
+                    SizedBox(height: 20.h),
+                    Center(
+                      child: RichText(
+                        text: TextSpan(
+                          text: AppLocalizations.of(context)!.alreadyHaveAccount,
+                          style: TextStyle(color: GlobalColors.black),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: AppLocalizations.of(context)!.login,
+                              style: TextStyle(
+                                color: GlobalColors.orange,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  context.go(AppRoute.login);
+                                },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _handleCreateAccount(WidgetRef ref, BuildContext context) {
+    ref.read(authProvider.notifier).registerSingleUser({
+      'email': emailController.text,
+      'first_name': firstNameController.text,
+      'last_name': lastNameController.text,
+      'password': passwordController.text,
+    }, context);
+  }
+}
