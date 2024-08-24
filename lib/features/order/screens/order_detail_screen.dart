d1afcff674957151cd6fdfe8292a4d7ecd58a2aa
Edward Victorhez <48755413+victorhez@users.noreply.github.com>
2024-08-24 21:37:11 +0100

    FIXED

--- lib/features/order/screens/order_detail_screen.dart
+++ lib/features/order/screens/order_detail_screen.dart
@@ -1,4 +1,4 @@
-import 'package:cached_network_image/cached_network_image.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate_hng11/features/auth/widgets/custom_app_bar.dart';
 import 'package:flutter_boilerplate_hng11/features/cart/utils/widget_extensions.dart';
