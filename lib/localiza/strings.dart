c01dbdba0c928cd8424ba45fb7ecadc4ae1e3954
omozuas <iyanuomozua@gmail.com>
2024-08-24 10:54:31 +0100

    new

--- lib/localiza/strings.dart
+++ lib/localiza/strings.dart
@@ -271,7 +271,7 @@ extension StringManager on BuildContext {
   static const String profileName = 'Chad Bosewick';
   static const String profileEmail = 'ChadBosew@gmail.com';
 
-  /// TODO: Put every new strings under this todo
+  /// tODO: Put every new strings under this todo
 
   static const String editOrganization = 'Edit Organization';
 
