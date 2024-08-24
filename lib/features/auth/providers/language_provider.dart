e8767279cf55afcd124332f5c3cf986f9798800c
David Ohimai <daveohimai@gmail.com>
2024-08-24 11:51:35 +0100

    feat: change language persisted

--- lib/features/auth/providers/language_provider.dart
+++ lib/features/auth/providers/language_provider.dart
@@ -1,13 +1,25 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
 import '../../../localiza/localiza_class.dart';
 
 class LanguageNotifier extends StateNotifier<Locale> {
-  LanguageNotifier() : super(LocalizationClass.english);
+  static const _languageKey = 'language_code';
+  final GetStorage _storage = GetStorage();
+
+  LanguageNotifier() : super(_loadSavedLocale());
+
+  static Locale _loadSavedLocale() {
+    final storage = GetStorage();
+    final savedLanguageCode = storage.read<String>(_languageKey) ??
+        LocalizationClass.english.languageCode;
+    return LocalizationClass.getLocale(savedLanguageCode);
+  }
 
   void setLanguage(String languageCode) {
     state = LocalizationClass.getLocale(languageCode);
+    _storage.write(_languageKey, languageCode);
   }
 }
 
