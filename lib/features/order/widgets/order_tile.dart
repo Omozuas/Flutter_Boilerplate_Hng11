64bce4c2f3892f2a6bf19c09812ab24b7ba60808
Jheezzy <horlajidey.olajide3@gmail.com>
2024-08-24 22:28:47 +0100

    refactor

--- lib/features/order/widgets/order_tile.dart
+++ lib/features/order/widgets/order_tile.dart
@@ -6,7 +6,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_boilerplate_hng11/utils/context_extensions.dart';
 
-
 class OrderTile extends StatelessWidget {
   const OrderTile({super.key, required this.order});
 
@@ -15,75 +14,73 @@ class OrderTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 16.0),
+      padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 24.0),
       child: GestureDetector(
         onTap: () {},
-        child: Stack(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 107.0,
-              padding: const EdgeInsets.only(
-                left: 20.0,
-                right: 11.0,
-                top: 10.0,
-                bottom: 8.0,
-              ),
-              decoration: BoxDecoration(
-                color: GlobalColors.white,
-                borderRadius: BorderRadius.circular(8.r),
-                border: Border.all(
-                  color: const Color(0xFFF7F7F7),
+        child: Container(
+          width: double.infinity,
+          height: 107.0,
+          padding: const EdgeInsets.only(
+            left: 20.0,
+            right: 20.0,
+            top: 10.0,
+            bottom: 8.0,
+          ),
+          decoration: BoxDecoration(
+            color: GlobalColors.white,
+            borderRadius: BorderRadius.circular(8.r),
+            border: Border.all(
+              color: const Color(0xFFF7F7F7),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${context.orderNumberText} ${order.number}',
+                          style: GoogleFonts.inter(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w600,
+                            color: const Color(0xFF71717A),
+                          ),
+                        ),
+                        SizedBox(height: 5.0.h),
+                        Text(
+                          '${order.deliveryDate}, ${order.deliveryTime}',
+                          style: TextStyle(
+                            fontSize: 12.sp,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/images/svg/product_listing/delivery.svg',
+                        ),
+                        SizedBox(width: 6.w),
+                        Text(
+                          order.deliveryText,
+                          style: TextStyle(
+                            fontSize: 12.sp,
+                            color: order.deliveryColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '${context.orderNumberText} ${order.number}',
-                        style: GoogleFonts.inter(
-                          fontSize: 14.sp,
-                          fontWeight: FontWeight.w600,
-                          color: const Color(0xFF71717A),
-                        ),
-                      ),
-                      const SizedBox(height: 5.0),
-                      Text(
-                        '${order.deliveryDate}, ${order.deliveryTime}',
-                        style: TextStyle(
-                          fontSize: 12.sp,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(
-                        'assets/images/svg/product_listing/delivery.svg',
-                      ),
-                      SizedBox(width: 6.w),
-                      Text(
-                        order.deliveryText,
-                        style: TextStyle(
-                          fontSize: 12.sp,
-                          color: order.deliveryColor,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              top: 16.0,
-              right: 20.0,
-              child: Container(
+              Container(
                 width: 92.0,
                 height: 79.0,
                 decoration: BoxDecoration(
@@ -95,8 +92,8 @@ class OrderTile extends StatelessWidget {
                 ),
                 child: Image.asset(order.image),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
