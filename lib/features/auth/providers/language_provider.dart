7a8aa6398aff405b4dd7e45af82add15e23a0602
Otonye <otonye.robinson@stu.cu.edu.ng>
2024-08-24 01:23:33 +0100

    Update language_provider.dart

--- lib/features/auth/providers/language_provider.dart
+++ lib/features/auth/providers/language_provider.dart
@@ -1,13 +1,23 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
 import '../../../localiza/localiza_class.dart';
+import '../../../services/service_locator.dart';
 
 class LanguageNotifier extends StateNotifier<Locale> {
-  LanguageNotifier() : super(LocalizationClass.english);
+  LanguageNotifier() : super(_loadInitialLanguage());
+
+  static final GetStorage _box = locator<GetStorage>();
+
+  static Locale _loadInitialLanguage() {
+    final savedLanguageCode = _box.read<String>('selectedLanguage');
+    return LocalizationClass.getLocale(savedLanguageCode ?? 'en');
+  }
 
   void setLanguage(String languageCode) {
     state = LocalizationClass.getLocale(languageCode);
+    _box.write('selectedLanguage', languageCode);
   }
 }
 
