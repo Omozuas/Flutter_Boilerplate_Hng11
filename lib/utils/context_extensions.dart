fe0068ff732aba85df768a3f8c8f2a492cc79f80
dumebiii <ezeukwucd13@gmail.com>
2024-08-24 13:32:39 +0100

    fix/Validators text localization

--- lib/utils/context_extensions.dart
+++ lib/utils/context_extensions.dart
@@ -10,4 +10,15 @@ extension Translation on BuildContext {
 // In your UI, use this : context.login (importing this context_extension file);
 
   String get login => 'Login';
+
+  String get nameRequired => "Name is required";
+  String get nameTwoCharacters => "Name must be at least 2 characters long";
+  String get emailRequired => "Email is required";
+  String get emailValid => "Enter a valid email address";
+  String get passwordRequired => "Password is required";
+  String get passwordEightCharacters =>
+      "Password must be at least 8 characters long";
+  String get passwordUpperCase =>
+      "Password must contain at least one uppercase letter";
+  String get passwordOneNumber => "Password must contain at least one number";
 }
