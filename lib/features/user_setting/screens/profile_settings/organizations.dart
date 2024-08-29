52872a1ae9dd00c741841f5e0f6b74682fa72df1
Otonye <otonye.robinson@stu.cu.edu.ng>
2024-08-29 21:34:15 +0100

    Update organizations.dart

--- lib/features/user_setting/screens/profile_settings/organizations.dart
+++ lib/features/user_setting/screens/profile_settings/organizations.dart
@@ -2,6 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate_hng11/features/auth/widgets/custom_app_bar.dart';
 import 'package:flutter_boilerplate_hng11/utils/custom_text_style.dart';
+import 'package:flutter_boilerplate_hng11/utils/global_colors.dart';
+import 'package:flutter_boilerplate_hng11/utils/widgets/custom_button.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -29,43 +31,38 @@ class _OrganizationState extends ConsumerState<Organization> {
       ),
       body: SafeArea(
         child: Column(
+            children: [
+        Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 24.w),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(height: 20.h)
-                  //TODO:CHANGE TO CORRECT STRING
-                  Text('Keep Track of your organizations here.'),
-                  SizedBox(height: 5.h),
-                  Text('Click the toggle buttons to switch through organizations',
-                    //  style: CustomTextStyle.regular(fontSize: 14.sp, ),
-                  ),
-                  SizedBox(height: 40.h,),
-                  Row(children: [
-                    Container(
-                      height: 77.h,
-                      width: 84.w,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.r),
-                        image: const DecorationImage(image: CachedNetworkImageProvider('imageUrl'), fit: BoxFit.cover),
-                      ),
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                    ),
-                  ],)
-                ],
-              ),
+            SizedBox(height: 20.h),
+            //TODO: CHANGE TO CORRECT STRING
+            Text('Keep Track of your organizations here.'),
+            SizedBox(height: 5.h),
+            Text('Click the toggle buttons to switch through organizations',
+              //  style: CustomTextStyle.regular(fontSize: 14.sp, ),
             ),
-            const Divider(),
-            SizedBox(height: 20.h,)
+            SizedBox(height: 40.h,),
+            OrganizationWidget(),
+            SizedBox(height: 20.h)
           ],
-
         ),
       ),
+      const Divider(),
+      SizedBox(height: 20.h,),
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.w),
+        // child: ListView.builder(itemBuilder: (BuildContext context, int index) {
+        //   return
+        // },
+        )
+    )],
+
+      ),
+    ),
     );
   }
 }
+
