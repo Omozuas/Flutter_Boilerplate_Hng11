5677818e31e89da5d7cb9e37f4a2d3e75d6229e2
Otonye <otonye.robinson@stu.cu.edu.ng>
2024-08-29 20:18:28 +0100

    Create organizations.dart

---
+++ lib/features/user_setting/screens/profile_settings/organizations.dart
@@ -0,0 +1,71 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate_hng11/features/auth/widgets/custom_app_bar.dart';
+import 'package:flutter_boilerplate_hng11/utils/custom_text_style.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+class Organization extends ConsumerStatefulWidget {
+  const Organization({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _OrganizationState();
+}
+
+class _OrganizationState extends ConsumerState<Organization> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar.simpleTitle(
+        //TODO:CHANGE TO CORRECT STRING
+        titleText:  AppLocalizations.of(context)!.members,
+        showDivider: false,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20.h)
+                  //TODO:CHANGE TO CORRECT STRING
+                  Text('Keep Track of your organizations here.'),
+                  SizedBox(height: 5.h),
+                  Text('Click the toggle buttons to switch through organizations',
+                    //  style: CustomTextStyle.regular(fontSize: 14.sp, ),
+                  ),
+                  SizedBox(height: 40.h,),
+                  Row(children: [
+                    Container(
+                      height: 77.h,
+                      width: 84.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12.r),
+                        image: const DecorationImage(image: CachedNetworkImageProvider('imageUrl'), fit: BoxFit.cover),
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                    ),
+                  ],)
+                ],
+              ),
+            ),
+            const Divider(),
+            SizedBox(height: 20.h,)
+          ],
+
+        ),
+      ),
+    );
+  }
+}
